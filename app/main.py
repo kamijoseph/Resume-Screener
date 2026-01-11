99cf5aadeed95c9eee9c21b920e9dbf8135558ff
Kami <josephkiarie561@gmail.com>
2026-01-11 13:20:12 +0300

    loading artifacts

--- app/main.py
+++ app/main.py
@@ -6,3 +6,23 @@ import nltk
 
 nltk.download("punkt")
 nltk.download("stopwords")
+
+# loading artifacts
+@st.cache_resource
+def load_artifacts(base_path):
+
+    # model
+    with open(f"{base_path}/model.pkl", "rb") as f:
+        model = pickle.load(f)
+    
+    # encoder
+    with open(f"{base_path}/encoder.pkl", "rb") as f:
+        encoder = pickle.load(f)
+
+    # vectorizer
+    with open(f"{base_path}/vectorizer.pkl", "rb") as f:
+        vectorizer = pickle.load(f)
+
+    return model, encoder, vectorizer
+
+model, encoder, vectorizer = load_artifacts("app/../resources")
